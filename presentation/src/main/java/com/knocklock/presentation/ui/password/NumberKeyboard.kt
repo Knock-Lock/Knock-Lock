dd530eb3a32ed8366497d2fac16dab1c213507bb
jaeryo2357 <jaeryo2357@naver.com>
2022-12-04 12:05:43 +0900

    [feat]: compose 의존성 추가

---
+++ presentation/src/main/java/com/knocklock/presentation/ui/password/NumberKeyboard.kt
@@ -0,0 +1,15 @@
+package com.knocklock.presentation.ui.password
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun KeyboardButton() {
+
+}
+
+@Preview
+@Composable
+fun KeyboardButtonPrev() {
+
+}
