cfec20370ad70ffefe7ef323d4c127d6865cd9cb
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-12-04 18:55:45 +0900

    [fix]: LazyColumn -> Column 변경

--- presentation/src/main/java/com/knocklock/presentation/ui/setting/credit/DeveloperList.kt
+++ presentation/src/main/java/com/knocklock/presentation/ui/setting/credit/DeveloperList.kt
@@ -1,9 +1,8 @@
 package com.knocklock.presentation.ui.setting.credit
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -14,15 +13,33 @@ import com.knocklock.presentation.R
 @Composable
 fun DeveloperList(
     modifier: Modifier = Modifier,
-    devList: List<Developer>
 ) {
-    LazyColumn(
+    Column(
         modifier = modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-        items(devList) { dev ->
-            DeveloperItem(developer = dev)
-        }
+        DeveloperItem(
+            developer = Developer(
+                "Daq",
+                painterResource(id = R.drawable.hyunkuk),
+                "안녕하슈"
+            )
+        )
+        DeveloperItem(
+            developer = Developer(
+                "Jaeryo",
+                painterResource(id = R.drawable.minuk),
+                "안녕하슈"
+            )
+        )
+        DeveloperItem(
+            developer = Developer(
+                "Hence",
+                painterResource(id = R.drawable.hyunsu),
+                "안녕하슈"
+            )
+        )
+
     }
 }
 
@@ -30,10 +47,5 @@ fun DeveloperList(
 @Preview
 @Composable
 private fun PreviewDeveloperList() {
-    val tmpDev = mutableListOf<Developer>().apply {
-        add(Developer("Daq", painterResource(id = R.drawable.hyunkuk), "안녕하슈"))
-        add(Developer("Jaeryo", painterResource(id = R.drawable.minuk), "안녕하슈"))
-        add(Developer("Hence", painterResource(id = R.drawable.hyunsu), "안녕하슈"))
-    }
-    DeveloperList(devList = tmpDev)
+    DeveloperList()
 }
