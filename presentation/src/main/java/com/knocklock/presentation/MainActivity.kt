dd63c54c84048be0b1a7282305b8614df2aa511a
Hyunkuk <boris0815@naver.com>
2022-12-05 13:41:45 +0900

    [feat]: Notification을 가져오기 위해서, permission 확인 로직 추가

--- presentation/src/main/java/com/knocklock/presentation/MainActivity.kt
+++ presentation/src/main/java/com/knocklock/presentation/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.knocklock.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.knocklock.presentation.lockscreen.LockNotiItem
-import com.knocklock.presentation.lockscreen.LockScreen
+import androidx.core.app.NotificationManagerCompat
 import com.knocklock.presentation.lockscreen.LockScreenRoute
 import com.knocklock.presentation.ui.theme.KnockLockTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!permissionGranted()) {
+            val intent = Intent(
+                "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"
+            )
+            startActivity(intent)
+        }
+
         setContent {
             KnockLockTheme {
                 LockScreenRoute()
             }
         }
+    }
+    private fun permissionGranted(): Boolean {
+        val sets: Set<String> = NotificationManagerCompat.getEnabledListenerPackages(this)
+        return sets.contains(packageName)
     }
 }
