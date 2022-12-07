34269828e15f939f870bd65ded24c9711d1daf96
jaeryo2357 <jaeryo2357@naver.com>
2022-12-08 01:57:52 +0900

    [feat]: #10 hilt 설정

--- presentation/src/main/java/com/knocklock/presentation/MainActivity.kt
+++ presentation/src/main/java/com/knocklock/presentation/MainActivity.kt
@@ -3,11 +3,11 @@ package com.knocklock.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.knocklock.presentation.lockscreen.LockNotiItem
-import com.knocklock.presentation.lockscreen.LockScreen
 import com.knocklock.presentation.lockscreen.LockScreenRoute
 import com.knocklock.presentation.ui.theme.KnockLockTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
