631966c0d812137f5382f709541fd71619da7da8
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-12-10 16:36:25 +0900

    [fix]: menu data class 필드 변경

--- presentation/src/main/java/com/knocklock/presentation/ui/setting/SettingMenu.kt
+++ presentation/src/main/java/com/knocklock/presentation/ui/setting/SettingMenu.kt
@@ -1,22 +1,39 @@
 package com.knocklock.presentation.ui.setting
 
+import androidx.annotation.StringRes
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import com.knocklock.presentation.R
 
 sealed class SettingMenu {
 
     data class SwitchMenu(
-        val title: String,
+        val type: MenuType,
         val isChecked: Boolean = false,
-        val route: String,
-        val onSwitchChanged: (Boolean) -> Unit
     ) : SettingMenu() {
         var checked by mutableStateOf(isChecked)
     }
 
     data class NormalMenu(
-        val title: String,
-        val route: String
+        val type: MenuType
     ) : SettingMenu()
+}
+
+enum class MenuType(
+    @StringRes val titleRes: Int,
+    @StringRes val routeRes: Int
+) {
+    ACTIVATE_PASSWORD(
+        R.string.activate_password,
+        R.string.activate_password_route
+    ),
+    CHANGE_PASSWORD(
+        R.string.chagne_password,
+        R.string.change_password_route
+    ),
+    CREDIT(
+        R.string.credit,
+        R.string.credit_route
+    )
 }
