a8df0ae79863d05105575b0f521c64d5414f8559
Hyunkuk <boris0815@naver.com>
2022-12-12 19:44:58 +0900

    [feat] : Room Database 인스턴스 주입

--- data/src/main/java/com/knocklock/data/source/local/AppDatabase.kt
+++ data/src/main/java/com/knocklock/data/source/local/AppDatabase.kt
@@ -1,8 +1,6 @@
 package com.knocklock.data.source.local
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.knocklock.data.source.local.lockscreen.NotificationDao
 import com.knocklock.data.source.local.lockscreen.NotificationEntity
@@ -17,23 +15,6 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun notificationDao(): NotificationDao
 
     companion object {
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        fun getDatabase(context: Context): AppDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            synchronized(this) {
-                val appDatabase = Room.databaseBuilder(
-                    context,
-                    AppDatabase::class.java,
-                    "KnockLockDatabase"
-                ).build()
-                INSTANCE = appDatabase
-                return appDatabase
-            }
-        }
+        const val AppDatabaseName = "KnockLockDatabase"
     }
 }
