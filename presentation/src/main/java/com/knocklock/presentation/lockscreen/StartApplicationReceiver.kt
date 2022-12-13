988509e2633f7198b5be640285550c7bd0497009
Hyunkuk <boris0815@naver.com>
2022-12-14 01:33:03 +0900

    [feat]: 앱 화면 켜짐 수신 브로드 캐스트 리시버 생성

---
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/StartApplicationReceiver.kt
@@ -0,0 +1,36 @@
+package com.knocklock.presentation.lockscreen
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+/**
+ * @Created by 김현국 2022/12/13
+ * @Time 4:00 PM
+ */
+class StartApplicationReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when (intent?.action) {
+            Intent.ACTION_SCREEN_ON -> {
+                startLockScreen(context)
+            }
+            Intent.ACTION_BOOT_COMPLETED -> {
+                startLockScreen(context)
+            }
+        }
+    }
+
+    private fun startLockScreen(context: Context?) {
+        if (context != null) {
+            try{
+                val intent = Intent(context, LockScreenActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                context.startActivity(intent)
+            }catch(e: Exception){
+                println(e.stackTraceToString())
+            }
+
+        }
+    }
+}
