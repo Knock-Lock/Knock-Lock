1a97eed637b62d341a32ff82af31ea2735a21a2d
Hyunkuk <boris0815@naver.com>
2022-12-15 18:35:28 +0900

    [fix]: 불필요한 로직 제거

--- presentation/src/main/java/com/knocklock/presentation/lockscreen/LockScreenActivity.kt
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/LockScreenActivity.kt
@@ -1,14 +1,9 @@
 package com.knocklock.presentation.lockscreen
 
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
 import android.view.MotionEvent
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.knocklock.presentation.ui.theme.KnockLockTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,12 +17,9 @@ class LockScreenActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        showWhenLockedAndTurnScreenOn()
-        val factory = LockScreenFactory(this)
-        val lockScreenViewModel = ViewModelProvider(this, factory).get(LockScreenViewModel::class.java)
         setContent {
             KnockLockTheme {
-                LockScreenRoute(lockScreenViewModel)
+                LockScreenRoute()
             }
         }
     }
@@ -40,26 +32,5 @@ class LockScreenActivity : ComponentActivity() {
             }
             else -> super.onTouchEvent(event)
         }
-    }
-    private fun showWhenLockedAndTurnScreenOn() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-            setTurnScreenOn(true)
-        } else {
-            window.addFlags(
-                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-                    or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-            )
-        }
-    }
-}
-
-class LockScreenFactory(
-    private val context: Context
-) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(LockScreenViewModel::class.java)) {
-            return LockScreenViewModel(context) as T
-        }
-        throw IllegalArgumentException("Not found ViewModel Class")
     }
 }
