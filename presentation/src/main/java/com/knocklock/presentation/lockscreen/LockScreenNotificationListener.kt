75300e597b62bff8156b83888d61664fd362b399
Hyunkuk <boris0815@naver.com>
2022-12-15 18:10:15 +0900

    [fix]: null로 표시되는 것을 막기 위한 빈 string 처리

--- presentation/src/main/java/com/knocklock/presentation/lockscreen/LockScreenNotificationListener.kt
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/LockScreenNotificationListener.kt
@@ -30,21 +30,23 @@ class LockScreenNotificationListener : NotificationListenerService() {
         if (sbn != null && !TextUtils.isEmpty(packageName)) {
             val notification: Notification = sbn.notification
             val extras = notification.extras
-            val title = extras.getString(Notification.EXTRA_TITLE) ?: ""
-            val text = extras.getString(Notification.EXTRA_TEXT) ?: ""
-            val subText = extras.getString(Notification.EXTRA_SUB_TEXT) ?:""
+            val title = extras.getString(Notification.EXTRA_TITLE).toString()
+            val text = extras.getString(Notification.EXTRA_TEXT).toString()
+            val subText = extras.getString(Notification.EXTRA_SUB_TEXT).toString()
             val smallIcon = notification.smallIcon
             val largeIcon = notification.getLargeIcon()
 
-            scope.launch {
-                insertNotificationUseCase(
-                    NotificationDomainModel(
-                        id = 0,
-                        title = title,
-                        subText = subText,
-                        text = text
+            if (title.isNotBlank() && text.isNotBlank()) {
+                scope.launch {
+                    notificationRepository.insertNotification(
+                        NotificationDomainModel(
+                            id = 0,
+                            title = title,
+                            subText = subText,
+                            text = text
+                        )
                     )
-                )
+                }
             }
         }
     }
