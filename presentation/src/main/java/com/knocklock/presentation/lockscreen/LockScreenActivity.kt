a74ffc12d2bc278303f22d82538719ecb4953533
Hyunkuk <boris0815@naver.com>
2022-12-20 15:58:52 +0900

    [feature] : statusbar 숨기기

--- presentation/src/main/java/com/knocklock/presentation/lockscreen/LockScreenActivity.kt
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/LockScreenActivity.kt
@@ -1,9 +1,15 @@
 package com.knocklock.presentation.lockscreen
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.MotionEvent
+import android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.knocklock.presentation.ui.theme.KnockLockTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,9 +20,11 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LockScreenActivity : ComponentActivity() {
+    private val windowInsetsController by lazy { WindowCompat.getInsetsController(window, window.decorView) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        hideSystemUI()
         setContent {
             KnockLockTheme {
                 LockScreenRoute()
@@ -32,5 +40,15 @@ class LockScreenActivity : ComponentActivity() {
             }
             else -> super.onTouchEvent(event)
         }
+    }
+
+    private fun hideSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            window.attributes.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        }
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+        window.statusBarColor = Color.TRANSPARENT
+        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
