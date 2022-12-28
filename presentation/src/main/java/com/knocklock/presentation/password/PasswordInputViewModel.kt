c33e69e1df61e3adbaa8ccb96498d7dc5c15205e
jaeryo2357 <jaeryo2357@naver.com>
2022-12-28 22:33:21 +0900

    [feat]: #59 PasswordInputState 생성

--- presentation/src/main/java/com/knocklock/presentation/password/PasswordInputViewModel.kt
+++ presentation/src/main/java/com/knocklock/presentation/password/PasswordInputViewModel.kt
@@ -1,28 +1,76 @@
 package com.knocklock.presentation.password
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class PasswordInputViewModel @Inject constructor(): ViewModel() {
-    val inputPassword = mutableStateOf("")
+class PasswordInputViewModel @Inject constructor() : ViewModel() {
+    var passwordInputState by mutableStateOf<PasswordInputState>(
+        PasswordInputState.PasswordNoneState("")
+    )
 
     companion object {
         private const val MAX_PASSWORD_LENGTH = 6
     }
 
     fun onClickTextButton(text: String) {
-        if (inputPassword.value.length >= MAX_PASSWORD_LENGTH) return
-        inputPassword.value += text
+        addPassword(text)
     }
 
     fun onClickKeyboardAction(action: KeyboardAction) {
-         when (action) {
-             KeyboardAction.BACK_SPACE -> {
-                 inputPassword.value = inputPassword.value.dropLast(1)
-             }
-         }
+        when (action) {
+            KeyboardAction.BACK_SPACE -> {
+                removeLastPassword()
+            }
+        }
+    }
+
+    private fun addPassword(newPasswordNumber: String) {
+        val state = passwordInputState
+        if (state.getPasswordLength() >= MAX_PASSWORD_LENGTH) return
+
+        passwordInputState = when (state) {
+            is PasswordInputState.PasswordNoneState -> {
+                state.copy(inputPassword = state.inputPassword + newPasswordNumber)
+            }
+            is PasswordInputState.PasswordConfirmState -> {
+                state.copy(inputPassword = state.inputPassword + newPasswordNumber)
+            }
+        }
+    }
+
+    private fun removeLastPassword() {
+        val state = passwordInputState
+        if (state.getPasswordLength() <= 0) return
+
+        passwordInputState = when (state) {
+            is PasswordInputState.PasswordNoneState -> {
+                state.copy(inputPassword = state.inputPassword.dropLast(1))
+            }
+            is PasswordInputState.PasswordConfirmState -> {
+                state.copy(inputPassword = state.inputPassword.dropLast(1))
+            }
+        }
+    }
+}
+
+sealed interface PasswordInputState {
+    fun getPasswordLength(): Int
+
+    data class PasswordNoneState(
+        val inputPassword: String
+    ) : PasswordInputState {
+        override fun getPasswordLength() = inputPassword.length
+    }
+
+    data class PasswordConfirmState(
+        val inputPassword: String,
+        val savedPassword: String
+    ) : PasswordInputState {
+        override fun getPasswordLength() = inputPassword.length
     }
 }
