d37b19ee485e73f79707c0fff449dae159d9a243
Hyunkuk <boris0815@naver.com>
2022-12-29 17:10:42 +0900

    [feat]: API 31 이상 접근성 서비스로 상태바 제어

---
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/DismissStatusBarService.kt
@@ -0,0 +1,49 @@
+package com.knocklock.presentation.lockscreen
+
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.content.Intent
+import android.os.Build
+import android.view.accessibility.AccessibilityEvent
+
+/**
+ * @Created by 김현국 2022/12/20
+ * @Time 3:51 PM
+ */
+class DismissStatusBarService : AccessibilityService() {
+
+    private val info by lazy { AccessibilityServiceInfo() }
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK
+        info.notificationTimeout = 100
+        serviceInfo = info
+    }
+
+    override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
+        dismissNotificationShade()
+    }
+
+    override fun onInterrupt() {
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        return super.onUnbind(intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disableSelf()
+    }
+
+    private fun dismissNotificationShade(): Boolean {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                return performGlobalAction(GLOBAL_ACTION_DISMISS_NOTIFICATION_SHADE)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return false
+    }
+}
