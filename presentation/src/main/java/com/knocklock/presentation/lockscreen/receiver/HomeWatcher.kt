aeafd0d1a7ff1184ab0ffd49214f5b5f5eaa3511
Hyunkuk <boris0815@naver.com>
2023-01-05 16:06:53 +0900

    [feat]: Systembar 클릭, 앱 잠금 receiver 추가

---
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/receiver/HomeWatcher.kt
@@ -0,0 +1,54 @@
+package com.knocklock.presentation.lockscreen.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+
+/**
+ * @Created by 김현국 2022/12/28
+ * @Time 2:47 PM
+ */
+class HomeWatcher(
+    private val context: Context,
+    private val intentFilter: IntentFilter = IntentFilter(Intent.ACTION_CLOSE_SYSTEM_DIALOGS),
+    private val systemBarListener: OnSystemBarPressedListener
+) {
+
+    private val systemBarEventReceiver: SystemBarEventReceiver
+
+    interface OnSystemBarPressedListener {
+        fun onSystemBarClicked()
+    }
+
+    init {
+        systemBarEventReceiver = SystemBarEventReceiver()
+    }
+
+    fun startWatch() {
+        context.registerReceiver(systemBarEventReceiver, intentFilter)
+    }
+
+    fun stopWatch() {
+        context.unregisterReceiver(systemBarEventReceiver)
+    }
+
+    inner class SystemBarEventReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+            if (action == Intent.ACTION_CLOSE_SYSTEM_DIALOGS) {
+                val reason = intent.getStringExtra(SYSTEM_DIALOG_REASON_KEY)
+                if (reason != null) {
+                    if (reason == SYSTEM_DIALOG_REASON_HOME_KEY || reason == SYSTEM_DIALOG_REASON_RECENT_APPS) {
+                        systemBarListener.onSystemBarClicked()
+                    }
+                }
+            }
+        }
+    }
+    companion object {
+        const val SYSTEM_DIALOG_REASON_KEY = "reason"
+        const val SYSTEM_DIALOG_REASON_RECENT_APPS = "recentapps"
+        const val SYSTEM_DIALOG_REASON_HOME_KEY = "homekey"
+    }
+}
