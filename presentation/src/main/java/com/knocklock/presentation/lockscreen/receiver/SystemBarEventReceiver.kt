a7752c50d4f6e40c891938ce6236cf673a1ce076
Hyunkuk <boris0815@naver.com>
2023-01-08 17:06:46 +0900

    [refactor]: 중복된 함수 제거

--- presentation/src/main/java/com/knocklock/presentation/lockscreen/receiver/SystemBarEventReceiver.kt
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/receiver/SystemBarEventReceiver.kt
@@ -13,42 +13,34 @@ class SystemBarEventReceiver(
     private val context: Context,
     private val intentFilter: IntentFilter = IntentFilter(Intent.ACTION_CLOSE_SYSTEM_DIALOGS),
     private val onSystemBarEventListener: OnSystemBarEventListener
-) {
-
-    private val systemBarEventReceiver: SystemBarEventReceiver
-
-    interface OnSystemBarEventListener {
-        fun onSystemBarClicked()
-    }
-
-    init {
-        systemBarEventReceiver = SystemBarEventReceiver()
-    }
+) : BroadcastReceiver() {
 
     fun registerReceiver() {
-        context.registerReceiver(systemBarEventReceiver, intentFilter)
+        context.registerReceiver(this, intentFilter)
     }
 
     fun unregisterReceiver() {
-        context.unregisterReceiver(systemBarEventReceiver)
+        context.unregisterReceiver(this)
     }
 
-    inner class SystemBarEventReceiver : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            val action = intent.action
-            if (action == Intent.ACTION_CLOSE_SYSTEM_DIALOGS) {
-                val reason = intent.getStringExtra(SYSTEM_DIALOG_REASON_KEY)
-                if (reason != null) {
-                    if (reason == SYSTEM_DIALOG_REASON_HOME_KEY || reason == SYSTEM_DIALOG_REASON_RECENT_APPS) {
-                        onSystemBarEventListener.onSystemBarClicked()
-                    }
+    override fun onReceive(p0: Context?, intent: Intent) {
+        val action = intent.action
+        if (action == Intent.ACTION_CLOSE_SYSTEM_DIALOGS) {
+            val reason = intent.getStringExtra(SYSTEM_DIALOG_REASON_KEY)
+            if (reason != null) {
+                if (reason == SYSTEM_DIALOG_REASON_HOME_KEY || reason == SYSTEM_DIALOG_REASON_RECENT_APPS) {
+                    onSystemBarEventListener.onSystemBarClicked()
                 }
             }
         }
     }
+
     companion object {
         const val SYSTEM_DIALOG_REASON_KEY = "reason"
         const val SYSTEM_DIALOG_REASON_RECENT_APPS = "recentapps"
         const val SYSTEM_DIALOG_REASON_HOME_KEY = "homekey"
     }
+}
+interface OnSystemBarEventListener {
+    fun onSystemBarClicked()
 }
