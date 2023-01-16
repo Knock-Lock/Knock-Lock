415c923df228890fe0d827d99197ed4cecb6fbbd
Hyunkuk <boris0815@naver.com>
2023-01-16 14:38:53 +0900

    [feat]: notification 관찰 로직 추가

--- presentation/src/main/java/com/knocklock/presentation/lockscreen/service/InitLockScreenView.kt
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/service/InitLockScreenView.kt
@@ -5,6 +5,7 @@ import android.content.pm.ActivityInfo
 import android.graphics.PixelFormat
 import android.graphics.Point
 import android.os.Build
+import android.service.notification.StatusBarNotification
 import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
@@ -23,6 +24,8 @@ import com.knocklock.presentation.lockscreen.LockScreenRoute
 import com.knocklock.presentation.lockscreen.rememberLockScreenStateHolder
 import com.knocklock.presentation.lockscreen.util.ComposeLifecycleOwner
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 /**
@@ -38,13 +41,21 @@ class InitLockScreenView(
     private val lifecycleOwner by lazy { ComposeLifecycleOwner() }
     private val composeViewModelStore by lazy { ViewModelStore() }
 
+    private val notificationArray = MutableStateFlow<Array<StatusBarNotification>>(emptyArray())
+
     init {
         createComposeLockScreenView()
     }
 
     private fun createComposeLockScreenView() {
         composeView.setContent {
-            val stateHolder = rememberLockScreenStateHolder(context = context)
+            val stateHolder = rememberLockScreenStateHolder(context = context).apply {
+                coroutineScope.launch {
+                    notificationArray.collectLatest { notificationArray ->
+                        updateNotificationArray(notificationArray)
+                    }
+                }
+            }
             val notificationUiState by stateHolder.notificationList.collectAsState()
             LockScreenRoute(notificationUiState, userSwipe = {
                 onComposeViewListener.remove(composeView)
@@ -116,6 +127,10 @@ class InitLockScreenView(
                 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             )
         return params
+    }
+
+    fun passActiveNotificationList(statusBarNotification: Array<StatusBarNotification>) {
+        notificationArray.value = statusBarNotification
     }
 }
 interface OnComposeViewListener {
