03c4a8e81530a45f65ad3a0575e6863b7d014869
Hyunkuk <boris0815@naver.com>
2023-01-16 14:37:11 +0900

    [refactor]: usecase제거 및 현재 활성화된 알림 가져오는 로직 추가

--- presentation/src/main/java/com/knocklock/presentation/lockscreen/LockScreenStateHolder.kt
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/LockScreenStateHolder.kt
@@ -1,20 +1,17 @@
 package com.knocklock.presentation.lockscreen
 
 import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.os.Build
+import android.service.notification.StatusBarNotification
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import com.knocklock.domain.usecase.notification.DeleteAllNotificationUseCase
-import com.knocklock.domain.usecase.notification.GetNotificationUseCase
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
-import dagger.hilt.android.EntryPointAccessors
-import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 /**
@@ -22,52 +19,83 @@ import javax.inject.Inject
  * @Time 3:26 PM
  */
 
-@EntryPoint
-@InstallIn(SingletonComponent::class)
-interface UseCaseEntryPoint {
-    fun getNotificationUseCase(): GetNotificationUseCase
-    fun deleteAllNotificationUseCase(): DeleteAllNotificationUseCase
-}
-
 class LockScreenStateHolder @Inject constructor(
     context: Context,
-    private val coroutineScope: CoroutineScope
+    val coroutineScope: CoroutineScope
 ) {
 
-    private val _notificationList: MutableStateFlow<NotificationUiState> = MutableStateFlow(NotificationUiState.Empty)
+    private val date by lazy { Date() }
+
+    private val _notificationList: MutableStateFlow<NotificationUiState> = MutableStateFlow(
+        NotificationUiState.Empty
+    )
     val notificationList = _notificationList.asStateFlow()
 
-    private val useCaseEntryPoint =
-        EntryPointAccessors.fromApplication(
-            context,
-            UseCaseEntryPoint::class.java
-        )
+    private val packageManager by lazy { context.packageManager }
 
-    val getNotificationUseCase = useCaseEntryPoint.getNotificationUseCase()
-    val deleteAllNotificationUseCase = useCaseEntryPoint.deleteAllNotificationUseCase()
+    fun updateNotificationArray(notificationArray: Array<StatusBarNotification>) {
+        val notificationUiState = NotificationUiState.Success(
+            notificationList = notificationArray.map { statusBarNotification ->
 
-    init {
-        deleteAllNotification()
-        getNotification()
-    }
+                var appTitle = ""
 
-    private fun getNotification() {
-        coroutineScope.launch {
-            getNotificationUseCase().collect { list ->
-                val notificationList = list.map {
-                    it.toModel()
+                val subText =
+                    statusBarNotification.notification.extras.getString(android.app.Notification.EXTRA_SUB_TEXT)
+                        .toString()
+                val title =
+                    statusBarNotification.notification.extras.getString(android.app.Notification.EXTRA_TITLE)
+                        .toString()
+                val content =
+                    statusBarNotification.notification.extras.getString(android.app.Notification.EXTRA_TEXT)
+                        .toString()
+
+                with(statusBarNotification.notification.extras) {
+                    val packageName = statusBarNotification.packageName
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        val applicationInfo: ApplicationInfo? = try {
+                            packageManager.getApplicationInfo(
+                                packageName,
+                                PackageManager.ApplicationInfoFlags.of(PackageManager.GET_META_DATA.toLong())
+                            )
+                        } catch (e: PackageManager.NameNotFoundException) {
+                            null
+                        }
+                        if (applicationInfo == null) {
+                            println("package name : unknown")
+                        } else {
+                            val appName = packageManager.getApplicationLabel(applicationInfo)
+                            appTitle = appName.toString()
+                        }
+                    } else {
+                        val applicationInfo: ApplicationInfo? = try {
+                            packageManager.getApplicationInfo(packageName, 0)
+                        } catch (e: PackageManager.NameNotFoundException) {
+                            null
+                        }
+                        if (applicationInfo == null) {
+                            println("package name : unknown")
+                        } else {
+                            val appName = packageManager.getApplicationLabel(applicationInfo)
+                            appTitle = appName.toString()
+                        }
+                    }
+                    println(getString(android.app.Notification.EXTRA_TITLE))
+                    println(getString(android.app.Notification.EXTRA_SUB_TEXT))
+                    println(getString(android.app.Notification.EXTRA_TEXT))
+                    println("appName : $appTitle")
                 }
-                _notificationList.value = NotificationUiState.Success(
-                    notificationList = notificationList
+
+                Notification(
+                    id = statusBarNotification.key.hashCode(),
+                    icon = statusBarNotification.notification.smallIcon,
+                    appTitle = if (subText == "null") appTitle else subText, // Bold title
+                    notiTime = "",
+                    title = title,
+                    content = content
                 )
             }
-        }
-    }
-
-    private fun deleteAllNotification() {
-        coroutineScope.launch {
-            deleteAllNotificationUseCase()
-        }
+        )
+        _notificationList.value = notificationUiState
     }
 }
 
