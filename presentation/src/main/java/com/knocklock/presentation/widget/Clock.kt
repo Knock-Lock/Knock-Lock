3f7bd68347db07d84aac4ccf16e501bc9fda8841
Hyun Su <86879099+KimHance@users.noreply.github.com>
2023-01-18 00:37:23 +0900

    [refact]: TextClock이용방식으로 widget변경

--- presentation/src/main/java/com/knocklock/presentation/widget/Clock.kt
+++ presentation/src/main/java/com/knocklock/presentation/widget/Clock.kt
@@ -1,73 +1,69 @@
 package com.knocklock.presentation.widget
 
+import android.graphics.Color
+import android.widget.TextClock
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.knocklock.presentation.util.*
-import java.util.*
+import androidx.compose.ui.viewinterop.AndroidView
+import com.knocklock.presentation.util.TimeFormat
+import com.knocklock.presentation.util.TimeVerticalFormat
+import com.knocklock.presentation.util.TimeWithNoSecondFormat
+import com.knocklock.presentation.util.TimeWithSecondFormat
 
 
 @Composable
 fun ClockWidget(
     modifier: Modifier = Modifier,
     timeFormat: TimeFormat,
-    currentTime: Date
 ) {
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = timeFormat.getTimeFormat(currentTime),
-            color = Color.White,
-            fontSize = 36.sp,
-        )
-        Text(
-            text = timeFormat.getDateFormat(currentTime),
-            color = Color.White,
-            fontSize = 12.sp
-        )
+        AndroidView(
+            factory = { context ->
+                TextClock(context).apply {
+                    format12Hour = timeFormat.timeFormat
+                    textSize = 36F
+                    setTextColor(Color.WHITE)
+                }
+            })
+        if (timeFormat is TimeVerticalFormat) {
+            AndroidView(factory = { context ->
+                TextClock(context).apply {
+                    format12Hour = timeFormat.minutesFormat
+                    textSize = 36F
+                    setTextColor(Color.WHITE)
+                }
+            })
+        }
+        AndroidView(factory = { context ->
+            TextClock(context).apply {
+                format12Hour = timeFormat.dateFormat
+                textSize = 12F
+                setTextColor(Color.WHITE)
+            }
+        })
     }
 }
 
 @Preview
 @Composable
 fun PreviewClockOne() {
-    ClockWidget(
-        timeFormat = KoreaFormat,
-        currentTime = Date(System.currentTimeMillis())
-    )
+    ClockWidget(timeFormat = TimeWithNoSecondFormat)
 }
 
 @Preview
 @Composable
 fun PreviewClockTwo() {
-    ClockWidget(
-        timeFormat = KoreaSecondFormat,
-        currentTime = Date(System.currentTimeMillis())
-    )
+    ClockWidget(timeFormat = TimeWithSecondFormat)
 }
 
 @Preview
 @Composable
-fun PreviewClockThree() {
-    ClockWidget(
-        timeFormat = EnglishFormat,
-        currentTime = Date(System.currentTimeMillis())
-    )
-}
-
-@Preview
-@Composable
-fun PreviewClockFour() {
-    ClockWidget(
-        timeFormat = EnglishSecondFormat,
-        currentTime = Date(System.currentTimeMillis())
-    )
+fun PreviewClockThird() {
+    ClockWidget(timeFormat = TimeVerticalFormat)
 }
