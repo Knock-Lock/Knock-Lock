b7075663fef83f67300020fef0e3e82d89fbfcb0
Hyunkuk <boris0815@naver.com>
2023-01-25 14:38:22 +0900

    [feat] : notification 그룹화 추가

--- presentation/src/main/java/com/knocklock/presentation/lockscreen/LockScreenNotiItem.kt
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/LockScreenNotiItem.kt
@@ -2,15 +2,22 @@ package com.knocklock.presentation.lockscreen
 
 import android.app.PendingIntent
 import android.graphics.drawable.Drawable
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -35,17 +42,89 @@ data class Notification(
     val title: String = "",
     val content: String = "",
     val intent: PendingIntent? = null
+)
+
+@Composable
+fun GroupLockNotiItem(
+    modifier: Modifier = Modifier,
+    notificationList: List<Notification>
 ) {
-    companion object {
-        val Test = Notification(
-            id = "",
-            drawable = null,
-            appTitle = "Kakao",
-            notiTime = "Now",
-            title = "KnockLock app 개발중",
-            content = "이거 너무나 재미가 있는걸~"
-        )
+    val notification = notificationList[0]
+    var clickableState by remember { mutableStateOf(false) }
+    var expandState by rememberSaveable { mutableStateOf(false) }
+    LaunchedEffect(notificationList.size) {
+        clickableState = notificationList.size >= 2
     }
+
+    Column(
+        modifier = Modifier.clickable(enabled = clickableState) {
+            expandState = !expandState
+        }
+    ) {
+        Box {
+            Column(
+                modifier = modifier,
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                LockNotiTop(
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp)
+                        .padding(top = 4.dp),
+                    drawable = notification.drawable,
+                    appTitle = notification.appTitle,
+                    time = notification.notiTime
+                )
+                LockNotiContent(
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp)
+                        .padding(bottom = 4.dp)
+                        .wrapContentHeight(),
+                    title = notification.title,
+                    content = notification.content
+                )
+            }
+            if (clickableState) {
+                Icon(
+                    modifier = Modifier.align(Alignment.CenterEnd).padding(end = 5.dp),
+                    imageVector = if (expandState)Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = null
+                )
+            }
+        }
+        AnimatedVisibility(visible = !expandState) {
+            Column {
+                if (notificationList.size == 2) {
+                    MoreNotifcation(modifier = Modifier.padding(horizontal = 15.dp).fillMaxWidth().height(7.dp))
+                } else if (notificationList.size >= 3) {
+                    MoreNotifcation(modifier = Modifier.padding(horizontal = 15.dp).fillMaxWidth().height(7.dp))
+                    MoreNotifcation(modifier = Modifier.padding(horizontal = 35.dp).fillMaxWidth().height(5.dp))
+                }
+            }
+        }
+        AnimatedVisibility(visible = expandState) {
+            Column(
+                modifier = Modifier.padding(top = 4.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                for (index in 1 until notificationList.size) {
+                    LockNotiItem(
+                        modifier = Modifier.background(color = Color(0xFFFAFAFA).copy(alpha = 0.95f), shape = RoundedCornerShape(10.dp)).clip(RoundedCornerShape(10.dp)),
+                        notification = notificationList[index]
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+        }
+    }
+}
+
+@Composable
+fun MoreNotifcation(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.background(color = Color(0xFFFAFAFA).copy(alpha = 0.9f), shape = RoundedCornerShape(bottomStart = 5.dp, bottomEnd = 5.dp)).clip(shape = RoundedCornerShape(bottomStart = 5.dp, bottomEnd = 5.dp))
+    ) {}
 }
 
 @Composable
@@ -53,26 +132,28 @@ fun LockNotiItem(
     modifier: Modifier = Modifier,
     notification: Notification
 ) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        LockNotiTop(
-            modifier = Modifier
-                .padding(horizontal = 10.dp)
-                .padding(top = 4.dp),
-            drawable = notification.drawable,
-            appTitle = notification.appTitle,
-            time = notification.notiTime
-        )
-        LockNotiContent(
-            modifier = Modifier
-                .padding(horizontal = 10.dp)
-                .padding(bottom = 4.dp)
-                .wrapContentHeight(),
-            title = notification.title,
-            content = notification.content
-        )
+    Column {
+        Column(
+            modifier = modifier,
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            LockNotiTop(
+                modifier = Modifier
+                    .padding(horizontal = 10.dp)
+                    .padding(top = 4.dp),
+                drawable = notification.drawable,
+                appTitle = notification.appTitle,
+                time = notification.notiTime
+            )
+            LockNotiContent(
+                modifier = Modifier
+                    .padding(horizontal = 10.dp)
+                    .padding(bottom = 4.dp)
+                    .wrapContentHeight(),
+                title = notification.title,
+                content = notification.content
+            )
+        }
     }
 }
 
@@ -133,21 +214,3 @@ fun LockNotiContent(
     }
 }
 
-@Preview
-@Composable
-fun PreviewLockNotiItem() {
-    KnockLockTheme {
-        val color = Color(red = 0xCC, blue = 0xCC, green = 0xCC)
-        LockNotiItem(
-            modifier = Modifier
-                .width(200.dp)
-                .height(70.dp)
-                .background(
-                    color = color,
-                    shape = RoundedCornerShape(4.dp)
-                )
-                .clip(RoundedCornerShape(4.dp)),
-            notification = Notification.Test
-        )
-    }
-}
