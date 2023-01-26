8bb267040260c6d0baa0ab329e315410cb4b3ced
jaeryo2357 <jaeryo2357@naver.com>
2023-01-27 00:45:06 +0900

    [feat]: #76 windowInset 설정

--- presentation/src/main/java/com/knocklock/presentation/MainActivity.kt
+++ presentation/src/main/java/com/knocklock/presentation/MainActivity.kt
@@ -7,12 +7,14 @@ import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.normal.TedPermission
@@ -28,7 +30,7 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         requestPermission()
 
         setContent {
@@ -37,7 +39,8 @@ class MainActivity : ComponentActivity() {
                 Scaffold { paddingValues ->
                     KnockLockNavHost(
                         modifier = Modifier
-                            .padding(paddingValues),
+                            .padding(paddingValues)
+                            .systemBarsPadding(),
                         navController = navController
                     )
                 }
