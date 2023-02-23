6d0efabea5d9537e94ebfcec8ebe5aecf2141026
Kim Hyun Su <86879099+KimHance@users.noreply.github.com>
2023-02-23 16:05:51 +0900

    [fix]: 리졸버 퍼미션 수정

--- presentation/src/main/java/com/knocklock/presentation/util/Extensions.kt
+++ presentation/src/main/java/com/knocklock/presentation/util/Extensions.kt
@@ -12,13 +12,16 @@ import android.provider.MediaStore
 @Suppress("DEPRECATION", "NewApi")
 fun Uri.parseBitmap(context: Context): Bitmap {
     context.grantUriPermission(context.packageName, this, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    val resolver = context.contentResolver.apply {
+        takePersistableUriPermission(this@parseBitmap, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    }
     return when (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) { // 28
         true -> {
-            val source = ImageDecoder.createSource(context.contentResolver, this)
+            val source = ImageDecoder.createSource(resolver, this)
             ImageDecoder.decodeBitmap(source)
         }
         else -> {
-            MediaStore.Images.Media.getBitmap(context.contentResolver, this)
+            MediaStore.Images.Media.getBitmap(resolver, this)
         }
     }
 }
