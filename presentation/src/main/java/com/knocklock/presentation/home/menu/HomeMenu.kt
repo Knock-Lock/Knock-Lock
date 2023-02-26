2111c16a2ba9398a6b06057c096fa0b9f09be48a
jaeryo2357 <jaeryo2357@naver.com>
2023-02-26 22:20:46 +0900

    [feature]: #92 home menu Icon 애니메이션 넣기

--- presentation/src/main/java/com/knocklock/presentation/home/menu/HomeMenu.kt
+++ presentation/src/main/java/com/knocklock/presentation/home/menu/HomeMenu.kt
@@ -1,9 +1,6 @@
 package com.knocklock.presentation.home.menu
 
-import androidx.annotation.StringRes
-import androidx.compose.material.icons.Icons
 import androidx.compose.ui.graphics.vector.ImageVector
-import com.knocklock.presentation.R
 import com.knocklock.presentation.icon.KnockLockIcons
 
 enum class HomeMenu(val icon: ImageVector) {
--- presentation/src/main/java/com/knocklock/presentation/home/menu/HomeMenuBar.kt
+++ presentation/src/main/java/com/knocklock/presentation/home/menu/HomeMenuBar.kt
@@ -1,14 +1,19 @@
 package com.knocklock.presentation.home.menu
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.Spring.StiffnessLow
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,8 +46,22 @@ fun HomeMenuItem(
     modifier: Modifier = Modifier,
     homeMenu: HomeMenu
 ) {
+    var size by remember { mutableStateOf(0.2f) }
+    val animateScale by animateFloatAsState(
+        targetValue = size,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioLowBouncy,
+            stiffness = StiffnessLow
+        )
+    )
+
+    LaunchedEffect(Unit) {
+        size = 1.0f
+    }
+
     Box(
         modifier = modifier
+            .scale(animateScale)
             .clip(CircleShape)
             .background(color = Color.Black.copy(alpha = 0.4f))
     ) {
