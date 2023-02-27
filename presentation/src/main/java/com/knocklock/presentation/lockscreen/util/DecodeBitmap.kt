f22dda960a2ff0240d7c6faa3ac0cfd639ad7800
Hyunkuk <boris0815@naver.com>
2023-02-27 17:53:03 +0900

    [feat] : Software allocator가 추가된 decoder로 Bitmap반환
     java.lang.IllegalArgumentException: Software rendering doesn't support hardware bitmaps 오류 해결

---
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/util/DecodeBitmap.kt
@@ -0,0 +1,31 @@
+package com.knocklock.presentation.lockscreen.util
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+
+/**
+ * @Created by 김현국 2023/02/27
+ */
+
+fun Uri.toBitmap(context: Context): Bitmap {
+    context.grantUriPermission(context.packageName, this, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        ImageDecoder.decodeBitmap(
+            ImageDecoder.createSource(context.contentResolver, this)
+        ) { decoder: ImageDecoder, _: ImageDecoder.ImageInfo?, _: ImageDecoder.Source? ->
+            decoder.isMutableRequired = true
+            decoder.allocator = ImageDecoder.ALLOCATOR_SOFTWARE
+        }
+    } else {
+        BitmapDrawable(
+            context.resources,
+            MediaStore.Images.Media.getBitmap(context.contentResolver, this)
+        ).bitmap
+    }
+}
