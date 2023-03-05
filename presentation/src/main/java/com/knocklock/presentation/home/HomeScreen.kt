b729a193e0c869c15737069188af0bc464f56539
jaeryo2357 <jaeryo2357@naver.com>
2023-03-06 00:19:08 +0900

    [feature]: #99 home 화면 palette 적용

--- presentation/src/main/java/com/knocklock/presentation/home/HomeScreen.kt
+++ presentation/src/main/java/com/knocklock/presentation/home/HomeScreen.kt
@@ -1,23 +1,31 @@
 package com.knocklock.presentation.home
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.graphics.drawable.toBitmap
+import androidx.palette.graphics.Palette
+import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
 import com.knocklock.domain.model.LockScreen
 import com.knocklock.domain.model.LockScreenBackground
 import com.knocklock.presentation.R
 import com.knocklock.presentation.home.menu.HomeMenu
 import com.knocklock.presentation.home.menu.HomeMenuBar
+import com.knocklock.presentation.ui.theme.KnockLockTheme
+import kotlinx.collections.immutable.toImmutableList
 
 @Composable
 fun HomeScreen(
@@ -39,36 +47,10 @@ fun HomeScreen(
                     onClickHomeMenu = onClickHomeMenu
                 )
             }
-            when (homeScreenUiState.lockScreen.background) {
-                is LockScreenBackground.DefaultWallPaper -> {
-                    Image(
-                        modifier = modifier,
-                        painter = rememberAsyncImagePainter(
-                            ImageRequest
-                                .Builder(LocalContext.current)
-                                .data(R.drawable.default_wallpaper)
-                                .build()
-                        ),
-                        contentDescription = null,
-                        contentScale = ContentScale.FillBounds,
-                        alpha = 0.4f
-                    )
-                }
-                is LockScreenBackground.LocalImage -> {
-                    Image(
-                        modifier = modifier,
-                        painter = rememberAsyncImagePainter(
-                            ImageRequest
-                                .Builder(LocalContext.current)
-                                .data((homeScreenUiState.lockScreen.background as LockScreenBackground.LocalImage).imageUri)
-                                .build()
-                        ),
-                        contentScale = ContentScale.FillBounds,
-                        contentDescription = null,
-                        alpha = 0.4f
-                    )
-                }
-            }
+            HomeContent(
+                modifier = Modifier.fillMaxSize(),
+                homeScreenUiState = homeScreenUiState
+            )
         }
     }
 }
@@ -76,9 +58,63 @@ fun HomeScreen(
 @Composable
 fun HomeContent(
     modifier: Modifier = Modifier,
-    lockScreen: LockScreen
+    homeScreenUiState: HomeScreenUiState.Success,
 ) {
-    Box(modifier = modifier) {
-        // implement LockScreen
+    val imagePainter = when (homeScreenUiState.lockScreen.background) {
+        is LockScreenBackground.DefaultWallPaper -> {
+            rememberAsyncImagePainter(
+                ImageRequest
+                    .Builder(LocalContext.current)
+                    .data(R.drawable.default_wallpaper)
+                    .allowHardware(false)
+                    .build()
+            )
+        }
+        is LockScreenBackground.LocalImage -> {
+            rememberAsyncImagePainter(
+                ImageRequest
+                    .Builder(LocalContext.current)
+                    .data((homeScreenUiState.lockScreen.background as LockScreenBackground.LocalImage).imageUri)
+                    .allowHardware(false)
+                    .build()
+            )
+        }
+    }
+
+    val palette = (imagePainter.state as? AsyncImagePainter.State.Success)?.let {
+        Palette.from((it.result.drawable.toBitmap())).generate()
+    }
+
+    val backgroundColor = palette?.mutedSwatch?.let { Color(it.rgb) } ?: Color.Transparent
+
+    Box(
+        modifier = modifier
+            .background(color = backgroundColor)
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxSize(0.6f)
+                .align(Alignment.Center),
+            painter = imagePainter,
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            alpha = 0.4f
+        )
+    }
+}
+
+@Preview
+@Composable
+fun HomeContentPrev() {
+    KnockLockTheme {
+        Surface {
+            HomeContent(
+                modifier = Modifier.fillMaxSize(),
+                homeScreenUiState = HomeScreenUiState.Success(
+                    lockScreen = LockScreen(LockScreenBackground.DefaultWallPaper),
+                    menuList = emptyList<HomeMenu>().toImmutableList()
+                )
+            )
+        }
     }
 }
