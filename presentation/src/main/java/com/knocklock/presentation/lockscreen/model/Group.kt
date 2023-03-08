ad491b34fdc2b1251cb1ed48c6f32984cc1f85b9
Hyunkuk <boris0815@naver.com>
2023-03-08 18:01:57 +0900

    [feat] : create presentation layer model

---
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/model/Group.kt
@@ -0,0 +1,17 @@
+package com.knocklock.presentation.lockscreen.model
+
+import com.knocklock.domain.model.Group as GroupModel
+/**
+ * @Created by 김현국 2023/03/07
+ */
+data class Group(
+    val key: String
+)
+
+fun Group.toModel() = GroupModel(
+    key = this.key
+)
+
+fun GroupModel.toModel() = Group(
+    key = this.key,
+)
