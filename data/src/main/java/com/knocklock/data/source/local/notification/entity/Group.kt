218f992858a10d90f90a1e353f6eff360e519f3a
Hyunkuk <boris0815@naver.com>
2023-03-08 17:57:21 +0900

    [refactor]  : update Entity class

--- data/src/main/java/com/knocklock/data/source/local/notification/entity/Group.kt
+++ data/src/main/java/com/knocklock/data/source/local/notification/entity/Group.kt
@@ -1,14 +1,12 @@
 package com.knocklock.data.source.local.notification.entity
 
-import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import androidx.room.Relation
 
 /**
  * @Created by 김현국 2023/03/06
  */
-@Entity
+@Entity(tableName = "group")
 data class Group(
     @PrimaryKey val key: String
 )
--- data/src/main/java/com/knocklock/data/source/local/notification/entity/Notification.kt
+++ data/src/main/java/com/knocklock/data/source/local/notification/entity/Notification.kt
@@ -7,12 +7,12 @@ import androidx.room.PrimaryKey
  * @Created by 김현국 2023/03/06
  */
 
-@Entity
+@Entity(tableName = "notification")
 data class Notification(
-    @PrimaryKey val id: String = "",
+    @PrimaryKey val postedTime: Long,
+    val id: String = "",
     val packageName: String = "",
     val appTitle: String = "",
-    val postedTime: String = "",
     val title: String = "",
     val content: String = "",
     val isClearable: Boolean = false,
