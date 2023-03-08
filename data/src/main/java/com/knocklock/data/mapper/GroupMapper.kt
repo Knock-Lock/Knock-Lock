a8b75319a12ae5ea5b1fd609ba74aeab1a5df2c6
Hyunkuk <boris0815@naver.com>
2023-03-08 17:57:00 +0900

    [refactor] : update Mapper

--- data/src/main/java/com/knocklock/data/mapper/GroupMapper.kt
+++ data/src/main/java/com/knocklock/data/mapper/GroupMapper.kt
@@ -1,7 +1,9 @@
 package com.knocklock.data.mapper
 
 import com.knocklock.data.source.local.notification.entity.Group
+import com.knocklock.data.source.local.notification.entity.GroupWithNotification
 import com.knocklock.domain.model.Group as GroupModel
+import com.knocklock.domain.model.GroupWithNotification as GroupWithNotificationModel
 
 /**
  * @Created by 김현국 2023/03/06
@@ -13,4 +15,9 @@ fun GroupModel.toEntity() = Group(
 
 fun Group.toModel() = GroupModel(
     key = this.key
+)
+
+fun GroupWithNotification.toModel() = GroupWithNotificationModel(
+    group = this.group.toModel(),
+    notifications = this.notifications.map { it.toModel() }
 )
