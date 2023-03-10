2b32e102b072f0329518f5d99f6258b71268eb44
Hyunkuk <boris0815@naver.com>
2023-03-10 16:42:45 +0900

    [refactor] : move function Location to RespotiroyImpl.class

--- data/src/main/java/com/knocklock/data/repository/NotificationRepositoryImpl.kt
+++ data/src/main/java/com/knocklock/data/repository/NotificationRepositoryImpl.kt
@@ -12,7 +12,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 /**
@@ -26,20 +25,37 @@ class NotificationRepositoryImpl @Inject constructor(
         groupDao.insertGroup(group = group.toEntity())
     }
 
-    override suspend fun insertNotifications(vararg notifications: Notification) {
-        withContext(Dispatchers.IO) {
-            notificationDao.insertNotifications(
-                *notifications.map { notification ->
-                    notification.toEntity()
-                }.toTypedArray()
-            )
-        }
+    override suspend fun getSizeWithNotificationId(id: String): Int {
+        return notificationDao.getNotificationSize(id)
     }
 
-    override fun getGroupWithNotifications(): Flow<List<GroupWithNotification>> {
+    override suspend fun insertNotifications(vararg notifications: Notification) {
+        notificationDao.insertNotifications(
+            *notifications.map { notification ->
+                notification.toEntity()
+            }.toTypedArray()
+        )
+    }
+
+    /**
+     * 모든 그룹과 그룹에 속한 Notification들을 가져오고 그룹의 속한 Notification들을 최근 시간으로 정렬합니다.
+     *
+     * 이후 그룹에 속한 Notification의 첫번째 값의 postedTime을 최근 시간순으로 Group을 정렬합니다.
+     * @return Flow<List<[GroupWithNotification]>>
+     */
+    override fun getGroupWithNotificationsWithSorted(): Flow<List<GroupWithNotification>> {
         return flow {
-            groupDao.getGroupWithNotificationsWithSorted().collect { groups ->
-                emit(groups.map { groupWithNotification -> groupWithNotification.toModel() })
+            groupDao.getGroupWithNotifications().collect { groupWithNotifications ->
+                emit(
+                    groupWithNotifications.filter {
+                        it.notifications.isNotEmpty()
+                    }.map { groupWithNotification ->
+                        val notifications = groupWithNotification.notifications.sortedByDescending { it.postedTime }
+                        groupWithNotification.copy(
+                            notifications = notifications
+                        ).toModel()
+                    }.sortedByDescending { it.notifications[0].postedTime }
+                )
             }
         }.flowOn(Dispatchers.IO)
     }
--- data/src/main/java/com/knocklock/data/source/local/notification/dao/GroupDao.kt
+++ data/src/main/java/com/knocklock/data/source/local/notification/dao/GroupDao.kt
@@ -7,10 +7,7 @@ import androidx.room.Query
 import androidx.room.Transaction
 import com.knocklock.data.source.local.notification.entity.Group
 import com.knocklock.data.source.local.notification.entity.GroupWithNotification
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 
 /**
  * @Created by 김현국 2023/03/06
@@ -26,32 +23,6 @@ interface GroupDao {
     @Transaction
     @Query("SELECT * FROM `Group`")
     fun getGroupWithNotifications(): Flow<List<GroupWithNotification>>
-
-    /**
-     * 모든 그룹과 그룹에 속한 Notification들을 가져오고 그룹의 속한 Notification들을 최근 시간으로 정렬합니다.
-     *
-     * 이후 그룹에 속한 Notification의 첫번째 값의 postedTime을 최근 시간순으로 Group을 정렬합니다.
-     * @return Flow<List<[GroupWithNotification]>>
-     */
-    @Transaction
-    @Query("")
-    fun getGroupWithNotificationsWithSorted(): Flow<List<GroupWithNotification>> {
-        return flow {
-            getGroupWithNotifications().collect { groupWithNotifications ->
-
-                emit(
-                    groupWithNotifications.filter {
-                        it.notifications.isNotEmpty()
-                    }.map { groupWithNotification ->
-                        val notifications = groupWithNotification.notifications.sortedByDescending { it.postedTime }
-                        groupWithNotification.copy(
-                            notifications = notifications
-                        )
-                    }.sortedByDescending { it.notifications[0].postedTime }
-                )
-            }
-        }.flowOn(Dispatchers.IO)
-    }
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertGroup(group: Group)
