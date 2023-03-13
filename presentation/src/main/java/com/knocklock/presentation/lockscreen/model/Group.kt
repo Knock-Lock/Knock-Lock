13c9f49b9067534d8883f3e1524677062eb17504
Hyunkuk <boris0815@naver.com>
2023-03-13 14:45:59 +0900

    [refactor] : data class에 Immutable 추가

--- presentation/src/main/java/com/knocklock/presentation/lockscreen/model/Group.kt
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/model/Group.kt
@@ -1,9 +1,11 @@
 package com.knocklock.presentation.lockscreen.model
 
+import androidx.compose.runtime.Immutable
 import com.knocklock.domain.model.Group as GroupModel
 /**
  * @Created by 김현국 2023/03/07
  */
+@Immutable
 data class Group(
     val key: String
 )
--- presentation/src/main/java/com/knocklock/presentation/lockscreen/model/GroupWithNotification.kt
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/model/GroupWithNotification.kt
@@ -1,9 +1,12 @@
 package com.knocklock.presentation.lockscreen.model
 
+import androidx.compose.runtime.Immutable
+
 /**
  * @Created by 김현국 2023/03/07
  */
 
+@Immutable
 data class GroupWithNotification(
     val group: Group,
     val notifications: List<Notification>
