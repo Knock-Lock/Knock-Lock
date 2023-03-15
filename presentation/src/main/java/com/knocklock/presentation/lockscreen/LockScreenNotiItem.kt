a246bd259ad669e849c2e271bfabf3b26438d665
Hyunkuk <boris0815@naver.com>
2023-03-15 16:38:36 +0900

    [feat] : notification 확장로직 추가

--- presentation/src/main/java/com/knocklock/presentation/lockscreen/LockScreenNotiItem.kt
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/LockScreenNotiItem.kt
@@ -4,6 +4,8 @@ import android.app.PendingIntent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -58,7 +60,13 @@ fun GroupLockNotiItem(
         }
     }
     Column(
-        modifier = modifier
+        modifier = modifier.clickable(
+            enabled = clickableState,
+            indication = null,
+            interactionSource = remember { MutableInteractionSource() }
+        ) {
+            expandableState = !expandableState
+        }
     ) {
         val lockNotiModifier = modifier
             .background(
@@ -296,6 +304,8 @@ fun LockNotiContent(
     ) {
         Text(
             title,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
             fontWeight = FontWeight.W700
         )
         Text(
