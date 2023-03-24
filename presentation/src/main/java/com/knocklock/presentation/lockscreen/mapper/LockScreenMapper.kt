1b6710bd4e5d91aa286e7202940d9957a5572792
Hyunkuk <boris0815@naver.com>
2023-03-24 17:49:11 +0900

    [feat]: LockScreenModel을 stable하게 해주기 위한 mapper 추가

---
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/mapper/LockScreenMapper.kt
@@ -0,0 +1,23 @@
+package com.knocklock.presentation.lockscreen.mapper
+
+import com.knocklock.presentation.lockscreen.model.LockScreen
+import com.knocklock.presentation.lockscreen.model.LockScreenBackground
+import com.knocklock.domain.model.LockScreen as LockScreenModel
+import com.knocklock.domain.model.LockScreenBackground as LockScreenBackgroundModel
+
+/**
+ * @Immutable 속성을 사용하기 위한 Domain to PresentationModel Mapper입니다.
+ * @return [LockScreen]
+ */
+fun LockScreenModel.toModel() = LockScreen(
+    background = this.background.toModel()
+)
+
+fun LockScreenBackgroundModel.toModel() = when (this) {
+    is LockScreenBackgroundModel.DefaultWallPaper -> {
+        LockScreenBackground.DefaultWallPaper
+    }
+    is LockScreenBackgroundModel.LocalImage -> {
+        LockScreenBackground.LocalImage(this.imageUri)
+    }
+}
