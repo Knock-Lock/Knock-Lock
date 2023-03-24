6f844e317ce408f27f3a0ce870d39942684ef365
Hyunkuk <boris0815@naver.com>
2023-03-24 19:18:38 +0900

    [feat]: ComposeView를 넣을 LockScreenActivity생성

---
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/LockScreenActivity.kt
@@ -0,0 +1,179 @@
+package com.knocklock.presentation.lockscreen
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.content.pm.ActivityInfo
+import android.graphics.PixelFormat
+import android.graphics.Point
+import android.os.Build
+import android.os.Bundle
+import android.os.IBinder
+import android.view.Gravity
+import android.view.View
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import androidx.compose.runtime.Recomposer
+import androidx.compose.ui.platform.AndroidUiDispatcher
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.compositionContext
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewTreeLifecycleOwner
+import androidx.lifecycle.ViewTreeViewModelStoreOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+import com.knocklock.presentation.lockscreen.service.LockScreenNotificationListener
+import com.knocklock.presentation.lockscreen.util.ComposeLifecycleOwner
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class LockScreenActivity : ComponentActivity() {
+
+    private var composeView: ComposeView? = null
+
+    private val manager by lazy {
+        this.applicationContext.packageManager
+    }
+
+    private val point by lazy { Point() }
+    private val composeViewModelStore by lazy { ViewModelStore() }
+    private val lifecycleOwner by lazy { ComposeLifecycleOwner() }
+    private var notificationListener: LockScreenNotificationListener? = null
+    private var mBound: Boolean = false
+    private val vm: LockScreenViewModel by viewModels()
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(p0: ComponentName?, service: IBinder) {
+            val binder = service as LockScreenNotificationListener.LocalBinder
+            notificationListener = binder.getService()
+            mBound = true
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            mBound = false
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("로드 : in onCreate")
+        getWindowSize()
+        composeView = ComposeView(this).also { view ->
+            initViewTreeOwner(view)
+            view.setContent {
+                LockScreenHost(
+                    onFinish = {
+                        windowManager.removeViewImmediate(composeView)
+                        finishAndRemoveTask()
+                    },
+                    onRemoveNotifications = { keys ->
+                        if (mBound) notificationListener?.cancelNotifications(keys)
+                    },
+                    vm = vm,
+                    packageManager = manager,
+                )
+            }
+        }
+
+        windowManager.addView(composeView, getWindowManagerLayoutParams())
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Intent(this, LockScreenNotificationListener::class.java).also { intent ->
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
+        mBound = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        notificationListener = null
+        composeView = null
+    }
+    private fun initViewTreeOwner(composeView: ComposeView) {
+        lifecycleOwner.apply {
+            performRestore(null)
+            handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+        }
+        ViewTreeLifecycleOwner.set(composeView, lifecycleOwner)
+        composeView.setViewTreeSavedStateRegistryOwner(lifecycleOwner)
+
+        ViewTreeViewModelStoreOwner.set(composeView) { composeViewModelStore }
+        val coroutineContext = AndroidUiDispatcher.CurrentThread
+        val runRecomposeScope = CoroutineScope(coroutineContext)
+        val recomposer = Recomposer(coroutineContext)
+        composeView.compositionContext = recomposer
+        runRecomposeScope.launch {
+            recomposer.runRecomposeAndApplyChanges()
+        }
+    }
+
+    private fun getWindowSize() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            point.x = windowManager.maximumWindowMetrics.bounds.width()
+            point.y = windowManager.maximumWindowMetrics.bounds.height()
+        } else {
+            windowManager.defaultDisplay.getRealSize(point)
+        }
+    }
+
+    private fun getWindowManagerLayoutParams(): WindowManager.LayoutParams {
+        val type: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.N_MR1) {
+            WindowManager.LayoutParams.TYPE_SYSTEM_ERROR
+        } else {
+            WindowManager.LayoutParams.TYPE_TOAST
+        }
+
+        val flags = (
+            WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS or
+                WindowManager.LayoutParams.FLAG_FULLSCREEN or
+                WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR or
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_OVERSCAN or
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+            )
+
+        val params = WindowManager.LayoutParams(
+            point.x,
+            point.y,
+            0,
+            0,
+            type,
+            flags,
+            PixelFormat.TRANSLUCENT,
+        )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            params.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        }
+
+        params.gravity = Gravity.TOP or Gravity.START
+        params.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        params.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            )
+        return params
+    }
+}
