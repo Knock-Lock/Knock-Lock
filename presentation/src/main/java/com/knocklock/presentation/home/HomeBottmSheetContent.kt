2769f6269c5e44d525ecf9371fca6a8aafe6499b
jaeryo2357 <jaeryo2357@naver.com>
2023-04-07 02:44:52 +0900

    [feat]: #80 BottomSheetContent 기본 ui 설정

--- presentation/src/main/java/com/knocklock/presentation/home/HomeBottmSheetContent.kt
+++ presentation/src/main/java/com/knocklock/presentation/home/HomeBottmSheetContent.kt
@@ -1,12 +1,28 @@
 package com.knocklock.presentation.home
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun HomeBottomSheetContent() {
-    //TODO Implement Sheet Content
-    Spacer(modifier = Modifier.fillMaxSize())
+fun HomeBottomSheetContent(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(top = 8.dp)
+    ) {
+        Spacer(
+            modifier = Modifier
+                .width(40.dp)
+                .height(6.dp)
+                .background(color = Color.LightGray, shape = RoundedCornerShape(3.dp))
+                .align(CenterHorizontally)
+        )
+    }
 }
--- presentation/src/main/java/com/knocklock/presentation/home/HomeRoute.kt
+++ presentation/src/main/java/com/knocklock/presentation/home/HomeRoute.kt
@@ -3,6 +3,7 @@ package com.knocklock.presentation.home
 import android.content.Intent
 import android.provider.MediaStore
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.BottomSheetValue
 import androidx.compose.material.rememberBottomSheetScaffoldState
@@ -13,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.knocklock.presentation.home.menu.HomeMenu
 import kotlinx.coroutines.launch
@@ -35,9 +37,15 @@ fun HomeRoute(
     BottomSheetScaffold(
         scaffoldState = bottomSheetScaffoldState,
         sheetContent = {
-            HomeBottomSheetContent()
+            HomeBottomSheetContent(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.8f)
+            )
         },
-        sheetBackgroundColor = Color.Black.copy(alpha = 0.8f)
+        sheetShape = RoundedCornerShape(12.dp),
+        sheetPeekHeight = 0.dp,
+        sheetBackgroundColor = Color.DarkGray,
     ) {
         HomeScreen(
             modifier = modifier,
