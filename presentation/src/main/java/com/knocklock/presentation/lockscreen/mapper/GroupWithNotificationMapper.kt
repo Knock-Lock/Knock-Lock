29fd21c7eae80892e00be9f728f08191d5dee265
Hyunkuk <boris0815@naver.com>
2023-05-17 14:38:43 +0900

    [feat] : domain model mapper 로직 추가

--- presentation/src/main/java/com/knocklock/presentation/lockscreen/mapper/GroupWithNotificationMapper.kt
+++ presentation/src/main/java/com/knocklock/presentation/lockscreen/mapper/GroupWithNotificationMapper.kt
@@ -1,8 +1,10 @@
 package com.knocklock.presentation.lockscreen.mapper
 
 import android.content.pm.PackageManager
+import com.knocklock.domain.model.Group
 import com.knocklock.presentation.lockscreen.model.GroupWithNotification
 import com.knocklock.presentation.lockscreen.model.Notification
+import com.knocklock.presentation.lockscreen.model.RemovedGroupNotification
 import com.knocklock.domain.model.GroupWithNotification as GroupWithNotificationModel
 
 /**
@@ -17,6 +19,13 @@ fun GroupWithNotificationModel.toModel(packageManager: PackageManager) = GroupWi
 fun GroupWithNotification.toModel() = GroupWithNotificationModel(
     group = this.group.toModel(),
     notifications = this.notifications.map { notification: Notification ->
+        notification.toModel()
+    },
+)
+
+fun RemovedGroupNotification.toModel() = GroupWithNotificationModel(
+    group = Group(this.key),
+    notifications = this.removedNotifications.map { notification: Notification ->
         notification.toModel()
     },
 )
