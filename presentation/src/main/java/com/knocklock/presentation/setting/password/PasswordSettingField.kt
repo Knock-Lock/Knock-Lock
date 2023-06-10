606422218e51e44d86b57fd2574f88a27d89cc3c
jaeryo2357 <jaeryo2357@naver.com>
2023-06-11 00:52:44 +0900

    [feat]: 비밀번호 설정 화면 숫자 입력 패트 UI 수정

--- presentation/src/main/java/com/knocklock/presentation/setting/password/PasswordSettingField.kt
+++ presentation/src/main/java/com/knocklock/presentation/setting/password/PasswordSettingField.kt
@@ -1,76 +1,70 @@
 package com.knocklock.presentation.setting.password
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Divider
-import androidx.compose.material3.LocalContentColor
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.knocklock.presentation.ui.theme.BrandGreenColor
 import com.knocklock.presentation.ui.theme.KnockLockTheme
 
 @Composable
 fun PasswordSettingField(
-    modifier: Modifier = Modifier,
-    number: String
+    number: String,
 ) {
-    Column(
-        modifier = modifier
-            .padding(horizontal = 8.dp),
-        horizontalAlignment = CenterHorizontally
-    ) {
-        Text(
-            text = number,
-            fontWeight = FontWeight.Bold,
-            fontSize = 16.sp
-        )
-
-        Divider(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp, bottom = 8.dp)
-                .height(1.dp),
-            color = LocalContentColor.current
-        )
-    }
+    val circleColor by animateColorAsState(
+        targetValue = if (number.isBlank()) Color.LightGray else BrandGreenColor,
+        label = "PasswordFieldColor"
+    )
+    Box(
+        modifier = Modifier
+            .background(
+                color = circleColor,
+                shape = CircleShape
+            )
+            .size(16.dp)
+    )
 }
 
 @Composable
 fun PasswordSettingFieldLayout(
-    modifier: Modifier = Modifier,
     password: String,
+    modifier: Modifier = Modifier,
     maxPasswordLength: Int = 5
 ) {
     Row(
         modifier = modifier,
-        horizontalArrangement = Arrangement.SpaceEvenly
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         for (index in 0..maxPasswordLength) {
             val number = password.getOrNull(index)?.toString() ?: ""
             key(index, number) {
                 PasswordSettingField(
-                    number = number,
-                    modifier = Modifier.weight(1f)
+                    number = number
                 )
             }
         }
     }
 }
 
-@Preview("DarkMode", uiMode = UI_MODE_NIGHT_YES)
 @Preview
 @Composable
 private fun PasswordSettingFieldPrev() {
@@ -83,7 +77,6 @@ private fun PasswordSettingFieldPrev() {
     }
 }
 
-@Preview("DarkMode", uiMode = UI_MODE_NIGHT_YES)
 @Preview
 @Composable
 private fun PasswordSettingFieldLayoutPrev() {
