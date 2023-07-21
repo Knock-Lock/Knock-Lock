49ae5c0bd6aebb12c93f99002d678b4eeae6bf67
Hyunkuk <boris0815@naver.com>
2023-07-21 12:08:57 +0900

    [feat]: flipper 의존성 추가

--- app/src/main/java/com/knocklock/KnockLockApplication.kt
+++ app/src/main/java/com/knocklock/KnockLockApplication.kt
@@ -1,7 +1,25 @@
 package com.knocklock
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.soloader.SoLoader
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class KnockLockApplication: Application()
+class KnockLockApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.addPlugin(DatabasesFlipperPlugin(this))
+            client.start()
+        }
+    }
+}
--- data/src/main/java/com/knocklock/data/source/local/notification/AppDatabase.kt
+++ data/src/main/java/com/knocklock/data/source/local/notification/AppDatabase.kt
@@ -1,7 +1,10 @@
 package com.knocklock.data.source.local.notification
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.knocklock.data.di.DatabaseModule.Companion.DB_NAME
 import com.knocklock.data.source.local.notification.dao.GroupDao
 import com.knocklock.data.source.local.notification.dao.NotificationDao
 import com.knocklock.data.source.local.notification.entity.Group
@@ -11,7 +14,7 @@ import com.knocklock.data.source.local.notification.entity.Notification
  * @Created by 김현국 2023/03/06
  */
 
-@Database(entities = [Group::class, Notification::class], version = 1)
+@Database(entities = [Group::class, Notification::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun groupDao(): GroupDao
     abstract fun notificationDao(): NotificationDao
